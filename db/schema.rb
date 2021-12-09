880b8891cebcba13dc5a0d224a6604a6d7f69a11
mackeee-orange <makino.aaa@gmail.com>
2021-12-09 19:39:50 +0900

    feat: Vote周りのtable, model, typeを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_181229) do
+ActiveRecord::Schema.define(version: 2021_12_09_093725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -59,7 +59,19 @@ ActiveRecord::Schema.define(version: 2021_12_03_181229) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "account_id", null: false
+    t.uuid "note_id", null: false
+    t.integer "value", null: false, comment: "そのNoteに対して良くないと思ったらマイナスの値,いいと思ったらプラスの値"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_votes_on_account_id"
+    t.index ["note_id"], name: "index_votes_on_note_id"
+  end
+
   add_foreign_key "note_taggings", "notes", on_delete: :cascade
   add_foreign_key "note_taggings", "tags", on_delete: :cascade
   add_foreign_key "notes", "accounts", column: "author_id", on_delete: :cascade
+  add_foreign_key "votes", "accounts", on_delete: :cascade
+  add_foreign_key "votes", "notes", on_delete: :cascade
 end
